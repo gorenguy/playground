a2c8463eea716a0e494674644b4d155dbf045008
guygo <goren@prospera.ag>
2025-05-13 20:27:18 +0300

    initial implementation

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://frontend:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/hello")
+def read_root():
+    return {"message": "Hello from FastAPI213"}
+
+
